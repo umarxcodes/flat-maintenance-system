94bb5435800443e109a8d823b9c4ea7033276f4c
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-09-03 12:27:20 +0500

    feat(backend): initialize backend structure and update configuration

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 45000,
-    });
-    console.log("MongoDB Connected Successfully");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully 🫰");
   } catch (error) {
-    console.error(`MongoDB Connection Error: ${error.message}`);
+    console.error("Error connecting to MongoDB:", error);
     process.exit(1);
   }
 };
